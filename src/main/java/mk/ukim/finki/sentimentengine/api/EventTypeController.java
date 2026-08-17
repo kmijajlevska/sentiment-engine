5f0c1871ff429289e1dad5dc42dab062c1910e45
Kristina Mijajlevska <kristina.mijajlevska@singular.uk>
2026-08-17 23:13:50 +0200

    SE-37 create initial UI

---
+++ src/main/java/mk/ukim/finki/sentimentengine/api/EventTypeController.java
@@ -0,0 +1,29 @@
+package mk.ukim.finki.sentimentengine.api;
+
+import mk.ukim.finki.sentimentengine.data.entity.EventType;
+import mk.ukim.finki.sentimentengine.service.EventTypeRegistry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author kristina
+ */
+@RestController
+@RequestMapping("event-types")
+public class EventTypeController {
+
+	private final EventTypeRegistry eventTypeRegistry;
+
+	public EventTypeController(EventTypeRegistry eventTypeRegistry) {
+		this.eventTypeRegistry = eventTypeRegistry;
+	}
+
+	@GetMapping
+	public List<EventType> getAllEventTypes() {
+		return eventTypeRegistry.getAllEventTypes();
+	}
+
+}
